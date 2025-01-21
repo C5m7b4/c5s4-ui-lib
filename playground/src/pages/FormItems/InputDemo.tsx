69f84698a68630c8991781ba4b8580eb63810bc6
Stephen <stephenn649@gmail.com>
2025-01-21 13:01:51 -0600

    Fixed bug in Input component: you can now search multiple times without having to clear the search

--- playground/src/pages/FormItems/InputDemo.tsx
+++ playground/src/pages/FormItems/InputDemo.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Input } from '../../../../src';
 import { iData, data } from '../../data/pivotData';
 
@@ -16,43 +16,61 @@ type filterText =
 
 const InputDemo = () => {
   const displayData: iData[] = data;
-  const [isSearching, setIsSearching] = useState<boolean>(false);
+  const [isSearching, setIsSearching] = useState<boolean | null>(false);
   const [filteredData, setFilteredData] = useState<filteredData[]>([]);
   const [text, setText] = useState<string>('');
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+
+  const displayRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (!isSearching) {
-      setFilteredData([]);
-      setText('');
-    } else if (text.length) {
+    console.log('howdy')
+    if (isSearching) {
+      if (displayRef.current) {
+        displayRef.current.setAttribute('data-display', 'active-search');
+      }
       setFilteredData(
         displayData.map((record: iData) => record[text as filterText]),
       );
+    } else {
+      if (displayRef.current) {
+        displayRef.current.setAttribute('data-display', 'inactive-search');
+      }
+      setFilteredData([]);
+      setText('');
     }
-    console.log(isSearching, text, filteredData);
-  }, [isSearching]);
+    setIsClicked(false)
+  }, [isSearching, setIsSearching, setText, isClicked]);
 
   return (
-    <div className='mt-5'>
+    <div className="mt-5">
       <div className="mb-3">
         Try searching for index, guid, isLoyalty, revenue, and/or cookieType
       </div>
-      <Input
-        text={text}
-        setText={setText}
-        setIsSearching={setIsSearching}
-      />
+      <Input text={text} setText={setText} setIsSearching={setIsSearching} setIsClicked={setIsClicked} />
       <div>
         <button
-          className="border mt-3 py-1 px-2 rounded-lg"
+          className="border mt-5 py-1 px-2 rounded-lg"
           onClick={() => setIsSearching(false)}
         >
           Clear Search
         </button>
       </div>
-      <div className='max-h-56'>
+      <div
+        ref={displayRef}
+        data-display="inactive-search"
+        className="data-[display=active-search]:border max-h-80 overflow-auto no-scrollbar mt-5"
+      >
         {isSearching && filteredData
-          ? filteredData.map((record, idx) => <div key={`record-${idx}`}>{record}</div>)
+          ? filteredData.map((record, idx) => (
+              <div
+                key={`record-${idx}`}
+                className="mb-1 w-full flex justify-center"
+              >
+                <div className="mr-2 w-8 text-right">{idx}:</div>
+                <div className="max-w-[310px] text-left">{record}</div>
+              </div>
+            ))
           : null}
       </div>
     </div>
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,10 +1,19 @@
 export interface InputProps {
   text: string;
-  setText: (t: string) => void;
-  setIsSearching: (t: boolean) => void;
+  setText: (str: string) => void;
+  setIsSearching: (bool: boolean) => void;
+  setIsClicked: (bool: boolean) => void;
+  width?: number;
 }
 
-const Input = ({ text, setText, setIsSearching }: InputProps) => {
+const Input = ({ text, setText, setIsSearching, width = 300, setIsClicked }: InputProps) => {
+
+  // subject to change
+  const handleClick = () => {
+    setIsSearching(true);
+    setIsClicked(true);
+  };
+
   return (
     <div>
       <div className="flex justify-center">
@@ -12,9 +21,9 @@ const Input = ({ text, setText, setIsSearching }: InputProps) => {
           type="text"
           value={text}
           onChange={(e) => setText(e.target.value)}
-          className="text-black w-[300px] px-2 py-1 rounded-l-lg shadow-md active:border-purple-500 outline-none"
+          className={`text-black w-[${width}px] px-2 py-1 rounded-l-lg shadow-md active:border-purple-500 outline-none`}
         />
-        <button onClick={() => setIsSearching(true)} className="border px-2 rounded-r-lg">
+        <button onClick={handleClick} className="border px-2 rounded-r-lg">
           Search
         </button>
       </div>
